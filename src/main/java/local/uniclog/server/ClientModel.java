69aecc52820d7fe4432ddfc262c40b7fc719363c
birewallg <g.sstrav@gmail.com>
2022-06-13 20:29:03 +0300

    add server side

---
+++ src/main/java/local/uniclog/server/ClientModel.java
@@ -0,0 +1,29 @@
+package local.uniclog.server;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.net.SocketAddress;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ClientModel {
+    private String ip;
+    private Integer port;
+    private String name;
+
+    private SocketAddress address;
+
+    @Override
+    public String toString() {
+        return "ClientModel{" +
+                "name='" + name + '\'' +
+                "ip='" + ip + '\'' +
+                ", port=" + port +
+                '}';
+    }
+}
